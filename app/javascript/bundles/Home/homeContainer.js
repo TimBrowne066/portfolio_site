d2b7e60f652de05b4d8ba5eb0fe94f102c6ea01c
Timothy Browne <timbrowne066@gmail.com>
2018-01-05 16:03:09 -0500

    project links

--- app/javascript/bundles/Home/homeContainer.js
+++ app/javascript/bundles/Home/homeContainer.js
@@ -3,6 +3,7 @@ import EyeEnc from '../../../assets/images/EYEENC.jpg'
 import OnBlast from '../../../assets/images/onblast.jpg'
 import Tourage from '../../../assets/images/TOURAGE.jpg'
 import Github from '../../../assets/images/github.png'
+import Www from '../../../assets/images/www.jpg'
 
 class HomeContainer extends React.Component {
   constructor(props) {
@@ -17,8 +18,8 @@ class HomeContainer extends React.Component {
     return (
       <div className="home_header">
       <div className="parallax1">
-        <h1>Hello, my name is Tim Browne</h1>
-        <h2>I am a Full Stack Web Developer</h2>
+        <h1>Hello, I'm Tim Browne</h1> '
+        <h2>I'm a Full Stack Web Developer</h2> '
       </div>
 
       <div className="content-box">
@@ -32,39 +33,38 @@ class HomeContainer extends React.Component {
       <div className="content-box">
       <div className="container">
         <div className="content">
-          <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
             <div className="content-overlay"></div>
             <img className="content-image" src={EyeEnc}/>
             <div className="content-details fadeIn-top">
               <h3>Eye Encounters</h3>
               <p>React-on-Rails - Bootstrap - JQuery</p>
               <a href="/"><img className="icon" src={Github}/></a>
+              <a href="/"><img className="icon" src={Www}/></a>
             </div>
-          </a>
         </div>
       </div>
       <div className="container">
         <div className="content">
-          <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
             <div className="content-overlay"></div>
             <img className="content-image" src={Tourage}/>
             <div className="content-details fadeIn-top">
               <h3>Tourage</h3>
               <p>React-on-Rails - Bootstrap - OAuth2 - AWS - Facebook API - Google Maps API</p>
+              <a href="/"><img className="icon" src={Github}/></a>
+              <a href="/"><img className="icon" src={Www}/></a>
             </div>
-          </a>
         </div>
       </div>
       <div className="container">
         <div className="content">
-          <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
             <div className="content-overlay"></div>
             <img className="content-image" src={OnBlast}/>
             <div className="content-details fadeIn-top">
               <h3>OnBlast!</h3>
               <p>ReactJS - Ruby on Rails - Devise - AWS - Bootstrap</p>
+              <a href="/"><img className="icon" src={Github}/></a>
+              <a href="/"><img className="icon" src={Www}/></a>
             </div>
-          </a>
         </div>
       </div>
       </div>
