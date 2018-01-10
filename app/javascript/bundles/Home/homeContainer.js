f9016b6cf1dd2a3f443f456940ae7cb5a0705f36
Timothy Browne <timbrowne066@gmail.com>
2018-01-09 22:39:24 -0500

    basic layout

--- app/javascript/bundles/Home/homeContainer.js
+++ app/javascript/bundles/Home/homeContainer.js
@@ -7,6 +7,9 @@ import Www from '../../../assets/images/www.jpg'
 import { Carousel, Image, Navbar, NavItem, MenuItem, NavDropdown, Nav, PanelGroup, Panel, Grid, Row, Col, Button, Well } from 'react-bootstrap';
 import NavBar from '../../Components/navBar'
 import Avatar from '../../../assets/images/avatar.jpg'
+import Email from "../../../assets/images/email.png"
+import LinkedIn from '../../../assets/images/linkedin.png'
+
 class HomeContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -22,8 +25,8 @@ class HomeContainer extends React.Component {
       <NavBar/>
 
         <div className="parallax1">
-          <h1>Hello, I'm Tim Browne</h1> '
-          <h2>I'm a Full Stack Web Developer</h2> '
+          <h1>Tim Browne</h1>
+          <h2>Full Stack Web Developer</h2>
         </div>
         <div className="content-box">
         <Grid>
@@ -33,13 +36,16 @@ class HomeContainer extends React.Component {
           <br/>
           <br/>
           <Row>
-            <Col className="avatar text-center" sm={6}>
-              <Image responsive thumbnail src={Avatar} />
-              <br/>
-              <br/>
+            <Col sm={12} lg={8} lgOffset={2}>
+            <Col className="text-center" sm={12} lg={6}>
+              <Image className="avatar" thumbnail src={Avatar} /><br/>
+              <a href="https://www.linkedin.com/in/timothydanielbrowne/"><img className="link-button" src={LinkedIn}/></a><a href="https://github.com/TimBrowne066"><img className="link-button" src={Github}/></a><a href="mailto:timbrowne066@gmail.com"><img className="link-button" src={Email}/></a>
             </Col>
-            <Col sm={6}>
-              <p className="text-center">Catchy bio.</p>
+            <Col sm={12} lg={6}>
+              <p className="text-center">I am a former semiconductor fabrications technician, photolithographer, optician, and punk enthusiast turned web developer.  Educated in the Colorado foothills, I am now living in Philadelphia.  I create dynamic web applications from the server up to the User Interface with a wide array of storied and cutting edge technologies.</p>
+              <p className="text-center">My work experience and education at Launch Academy here in Philadelphia have provided me with a sizeable skillset that I continue to expand.  My current repertoire of languages and libraries includes Ruby, C#, JavaScript, HTML5, CSS3, Rails, NodeJS, ReactJS, Sass, Bootstrap, JQuery, and many others.</p>
+              <p className="text-center">I am currently seeking role as a web or software developer where I can continue to refine my skills, learn and implement new technologies, and have a blast doing it!</p>
+            </Col>
             </Col>
           </Row>
           </Grid>
@@ -59,8 +65,8 @@ class HomeContainer extends React.Component {
                       <div className="content-details fadeIn-top">
                         <h3>Eye Encounters</h3>
                         <p>React-on-Rails - Bootstrap - JQuery</p>
-                        <a href="/"><img className="icon" src={Github}/></a>
-                        <a href="/"><img className="icon" src={Www}/></a>
+                        <a href="https://github.com/TimBrowne066/EyeEnc"><img className="icon" src={Github}/></a>
+                        <a href="http://www.eyeencounters.net/"><img className="icon" src={Www}/></a>
                       </div>
                   </div>
                 </div>
@@ -73,8 +79,8 @@ class HomeContainer extends React.Component {
                     <div className="content-details fadeIn-top">
                       <h3>Tourage</h3>
                       <p>React-on-Rails - Bootstrap - OAuth2 - AWS - Facebook API - Google Maps API</p>
-                      <a href="/"><img className="icon" src={Github}/></a>
-                      <a href="/"><img className="icon" src={Www}/></a>
+                      <a href="https://github.com/TimBrowne066/tourage"><img className="icon" src={Github}/></a>
+                      <a href="http://tourageapp.herokuapp.com/"><img className="icon" src={Www}/></a>
                     </div>
                 </div>
               </div>
@@ -87,8 +93,8 @@ class HomeContainer extends React.Component {
                     <div className="content-details fadeIn-top">
                       <h3>OnBlast!</h3>
                       <p>ReactJS - Ruby on Rails - Devise - AWS - Bootstrap</p>
-                      <a href="/"><img className="icon" src={Github}/></a>
-                      <a href="/"><img className="icon" src={Www}/></a>
+                      <a href="https://github.com/DRBragg/on-blast"><img className="icon" src={Github}/></a>
+                      <a href="https://on-blast.herokuapp.com/"><img className="icon" src={Www}/></a>
                     </div>
                 </div>
               </div>
