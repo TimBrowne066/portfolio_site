43661ce66a25bdf8a25fa590c4fe465fb48d7995
Timothy Browne <timbrowne066@gmail.com>
2018-02-15 17:10:12 -0500

    update

--- app/javascript/bundles/Home/homeContainer.js
+++ app/javascript/bundles/Home/homeContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import EyeEnc from '../../../assets/images/EYEENC.jpg'
-import OnBlast from '../../../assets/images/onblast.jpg'
-import Tourage from '../../../assets/images/TOURAGE.jpg'
+import Agile from '../../../assets/images/AGILE.jpg';
+import EyeEnc from '../../../assets/images/EYEENC.jpg';
+import OnBlast from '../../../assets/images/onblast.jpg';
+import Tourage from '../../../assets/images/TOURAGE.jpg';
 import Github from '../../../assets/images/github.png'
 import Www from '../../../assets/images/www.jpg'
 import { Carousel, Image, Navbar, NavItem, MenuItem, NavDropdown, Nav, PanelGroup, Panel, Grid, Row, Col, Button, Well } from 'react-bootstrap';
@@ -29,35 +30,34 @@ class HomeContainer extends React.Component {
           <h2>Full Stack Web Developer</h2>
         </div>
         <div className="content-box">
-        <Grid>
-          <Row>
             <h3 className="text-center">About me</h3>
-          </Row>
           <br/>
           <br/>
-          <Row>
-            <Col sm={12} lg={8} lgOffset={2}>
-            <Col className="text-center" sm={12} lg={6}>
+
               <Image className="avatar" thumbnail src={Avatar} /><br/>
               <a href="https://www.linkedin.com/in/timothydanielbrowne/"><img className="link-button" src={LinkedIn}/></a><a href="https://github.com/TimBrowne066"><img className="link-button" src={Github}/></a><a href="mailto:timbrowne066@gmail.com"><img className="link-button" src={Email}/></a>
-            </Col>
-            <Col sm={12} lg={6}>
+
               <p className="text-center">I am a former semiconductor fabrications technician, photolithographer, optician, and punk enthusiast turned web developer.  Educated in the Colorado foothills, I am now living in Philadelphia.  I create dynamic web applications from the Database up to the User Interface with a wide array of storied and cutting edge technologies.</p>
               <p className="text-center">My work experience and education at Launch Academy here in Philadelphia have provided me with a sizeable skillset that I continue to expand.  My current repertoire of languages and libraries includes Ruby, C#, JavaScript, HTML5, CSS3, Rails, NodeJS, ReactJS, Sass, Bootstrap, JQuery, SQL, Amazon Web Services, Google Cloud Platform, and many others.</p>
               <p className="text-center">I am currently seeking a role as a web or software developer where I can continue to refine my skills, learn and implement new technologies, and have a blast doing it!</p>
-            </Col>
-            </Col>
-          </Row>
-          </Grid>
         </div>
 
         <div className="parallax2">
           <h2 className="projects-header">Projects</h2>
         </div>
           <div className="content-box">
-          <Grid>
-            <Row className="text-center">
-              <Col className="text-center" sm={12} md={12} lg={8} lgOffset={2}>
+                <div className="container text-center">
+                  <div className="content">
+                      <div className="content-overlay"></div>
+                      <img className="content-image" src={Agile}/>
+                      <div className="content-details fadeIn-top">
+                        <h3>Agile Hires</h3>
+                        <p>React - Rails - Devise - PostgreSQL</p>
+                        <a href="https://github.com/TimBrowne066/AHSite"><img className="icon" src={Github}/></a>
+                        <a href="http://www.agilehires.com/"><img className="icon" src={Www}/></a>
+                      </div>
+                  </div>
+                </div>
                 <div className="container text-center">
                   <div className="content">
                       <div className="content-overlay"></div>
@@ -70,8 +70,6 @@ class HomeContainer extends React.Component {
                       </div>
                   </div>
                 </div>
-              </Col>
-              <Col sm={12} md={12} lg={8} lgOffset={2}>
               <div className="container">
                 <div className="content">
                     <div className="content-overlay"></div>
@@ -84,8 +82,6 @@ class HomeContainer extends React.Component {
                     </div>
                 </div>
               </div>
-              </Col>
-            <Col sm={12} md={12} lg={8} lgOffset={2}>
               <div className="container">
                 <div className="content">
                     <div className="content-overlay"></div>
@@ -98,9 +94,6 @@ class HomeContainer extends React.Component {
                     </div>
                 </div>
               </div>
-              </Col>
-            </Row>
-            </Grid>
           </div>
       </div>
     );
